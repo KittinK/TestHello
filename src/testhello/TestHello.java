45e8923b0526cc7bc8fc7d7edbfae291d5c269ae
KittinK <bewhammer70@gmail.com>
2022-03-25 14:39:28 +0700

    third commit

--- src/testhello/TestHello.java
+++ src/testhello/TestHello.java
@@ -16,6 +16,8 @@ public class TestHello {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World");
+        System.out.println("Welcome to Thailand");
+        System.out.println("Have a Good Trip");
     }
     
 }
